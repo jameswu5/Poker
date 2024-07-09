71ebbf217690762a61aeaa60bab875902c88fa49
James Wu <jameswu144@gmail.com>
2024-07-09 12:42:21 +0100

    create initial files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Poker;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World.");
+    }
+}

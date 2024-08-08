29f84863238400b54b39d84452d8d910c8fd67aa
James Wu <jameswu144@gmail.com>
2024-08-08 17:03:44 +0100

    process image

---
+++ UI/Settings.cs
@@ -0,0 +1,12 @@
+using System;
+using Raylib_cs;
+
+namespace Poker.UI;
+
+public static class Settings
+{
+    public static class Palette
+    {
+        public static readonly Color White = new(255, 255, 255, 255);
+    }
+}

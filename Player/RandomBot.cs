a3d798aebcbd1f78e4d1fce535c633c7b7d4684a
James Wu <jameswu144@gmail.com>
2024-08-23 15:38:20 +0100

    fix RandomBot bug

--- Player/RandomBot.cs
+++ Player/RandomBot.cs
@@ -6,6 +6,8 @@ public class RandomBot : Bot
 {
     public const string Name = "RandomBot";
 
+    public const int FoldPercentage = 20;
+
     private readonly Random rng;
 
     public RandomBot(int chips, Core.Pot pot) : base(Name, chips, pot)
@@ -14,16 +16,16 @@ public class RandomBot : Bot
     }
 
     public override void ChooseMove()
-    {        
+    {
         // For now, choose a random move between fold and call
-        int choice = rng.Next(0, 2);
-        if (choice == 0)
+        int choice = rng.Next(100);
+        if (choice < FoldPercentage)
         {
             chosenMove = new Core.Fold();
         }
         else
         {
-            chosenMove = new Core.Call(0);
+            chosenMove = new Core.Call(GetAmountToCall());
         }
         moveFound = true;
     }
